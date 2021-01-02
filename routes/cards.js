5925f034683d343fa9aa125f1144d94fe63f1b0c
Alex Semochkin <fennyflop@gmail.com>
2021-01-02 15:41:06 +0300

    commit

---
+++ routes/cards.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const fs = require('fs').promises;
+const path = require('path');
+
+const dataPath = path.join(__dirname, '../data/cards.json');
+
+router.get('/cards', (req, res) => {
+  fs.readFile(dataPath)
+    .then((data) => {
+      res.status(200).send(JSON.parse(data));
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    })
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const fs = require('fs').promises;
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const dataPath = path.join(__dirname, '../data/users.json');
+
+router.get('/users', (req, res) => {
+  fs.readFile(dataPath)
+    .then((users) => {
+      res.status(200).send(JSON.parse(users));
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    })
+})
+
+router.get('/users/:id', (req, res) => {
+  fs.readFile(dataPath)
+    .then((data) => {
+      const users = JSON.parse(data);
+      users.forEach((user) => {
+        if (user._id === req.params.id) {
+          res.status(200).send(user);
+          return;
+        };
+      })
+      res.status(404).send({ "message": "Нет пользователя с таким id" })
+    })
+})
+
+// .then((data) => {
+//   const users = JSON.parse(data);
+//   users.forEach((user) => {
+//     if (user._id === req.params.id) {
+//       res.status(200).send(user);
+//       return;
+//     };
+//   })
+// })
+
+module.exports = router;
